cfb5458f8769ab040cbda7bb1bb4f82137b2090e
Isaac Oseas <oseasisaac91@gmail.com>
2018-10-19 15:40:40 +0000

    automatically backed up by learn

--- lib/my_select.rb
+++ lib/my_select.rb
@@ -1,3 +1,14 @@
 def my_select(collection)
-  collection.select {|item| yield(i)}
+
+  i = 0 
+  new_col = []
+  while i < collection.length
+    item =collection[i]
+    if yield(item).include? item
+      new_col << item
+    end 
+  
+    i +=1
+  end 
+  new_col
 end
